c350d2d7df00cb63d583826d39c79a898fb0a0b4
Jesus Gollonet <jesusgollonet@gmail.com>
2017-05-18 10:48:50 +0200

    Change switchcase of global eslint

--- other/.eslintrc.js
+++ other/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     sourceType: "module"
   },
   rules: {
-    indent: ["error", 4],
+    indent: ["error", 4, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "always"],
