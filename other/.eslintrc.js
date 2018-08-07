2af083cd72e2668a0582b7ee778662378ddf2591
Jesus Gollonet <jesusgollonet@gmail.com>
2018-08-07 08:06:31 +0200

    eslint defaults

--- other/.eslintrc.js
+++ other/.eslintrc.js
@@ -3,16 +3,16 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
   parserOptions: {
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
-    indent: ["error", 4, { SwitchCase: 1 }],
-    "linebreak-style": ["error", "unix"],
-    semi: ["error", "always"],
-    "no-console": "off"
-  }
+    indent: ['error', 4, {SwitchCase: 1}],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'no-console': 'off',
+  },
 };
