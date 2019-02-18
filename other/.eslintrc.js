5c7658f5e67ee816bd10094802d78c632e67d872
Jesus Gollonet <jesusgollonet@gmail.com>
2019-02-18 11:45:53 +0100

    eslint 2017 ecma version

--- other/.eslintrc.js
+++ other/.eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    sourceType: 'module',
-  },
-  rules: {
-    indent: ['error', 4, {SwitchCase: 1}],
-    'linebreak-style': ['error', 'unix'],
-    semi: ['error', 'always'],
-    'no-console': 'off',
-  },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: '2017',
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'linebreak-style': ['error', 'unix'],
+        semi: ['error', 'always'],
+        'no-console': 'off'
+    }
 };
